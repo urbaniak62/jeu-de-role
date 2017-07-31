765df8c75c58b7ece58e02e7169b95fb5b92676e
urbaniak <urbaniaknicolas62@gmail.com>
2017-07-31 16:05:18 +0200

    fin

--- js/main.js
+++ js/main.js
@@ -46,3 +46,48 @@ alert(adv2.nom + " en profite et attaque " + hero.nom + " 3 fois " );
 hero.santée=hero.santée-adv2.attaque *3;
 alert("la santé de " + hero.nom + " est de " + hero.santée);
 hero.getName();
+
+/*constructeur----------MAGICIEN------------------------*/
+
+
+function magicien (nom , attaque , defense , santée , mana){
+  this.nom=nom,
+  this.attaque=attaque,
+  this.defense=defense,
+  this.santée=santée,
+  this.mana=mana,
+  this.getName= function (){
+
+  alert("nom : " + this.nom + " -puissance d'attaque : " + this.attaque + " -defense : " + this.defense + " -santée : " + this.santée + " -mana =" + this.mana);
+
+};
+}
+
+/*--------------------objet MAGICIEN------------------*/
+
+alert("un soigneur connu de tout le monde passé par la, c etait")
+
+var soigneur=new magicien( "merlin", 15 , 8 , 100 , 40);
+
+soigneur.getName();
+
+alert("le " + adv1.nom + " le " + adv2.nom + " attaque notre "
+ + soigneur.nom);
+
+ soigneur.santée= soigneur.santée - (adv1.attaque + adv2.attaque);
+ soigneur.getName();
+
+if (soigneur.santée <100){
+  soigneur.mana=soigneur.mana -10;
+  soigneur.santée=soigneur.santée +10;
+  alert("merlin se soigne ca vie est de " + soigneur.santée);
+  alert("malheureusement en se soignant il perd 10 point de mana et ne lui en reste plus que " + soigneur.mana);
+
+}else if (soigneur.mana<=10) {
+  alert("merlin n'a plus assez de mana");
+}
+else {
+  alert("c'est chaud");
+}
+alert("il reste a merlin : ")
+soigneur.getName();
